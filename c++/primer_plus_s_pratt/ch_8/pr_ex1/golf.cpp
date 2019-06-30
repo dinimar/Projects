382a7095d56dab6a2ebc47ef89a5f7ba69a23b5f
Dinir Imameev <dinir.imameev@gmail.com>
2019-06-30 18:58:21 +0500

    feat(chapter_8): add pr_ex1

---
+++ c++/primer_plus_s_pratt/ch_8/pr_ex1/golf.cpp
@@ -0,0 +1,33 @@
+#ifndef GOLF_H
+#define GOLF_H
+#include <golf.h>
+#endif
+
+#include <cstring>
+#include <iostream>
+
+using namespace std;
+
+void setgolf(golf &g, const char *name, int hc)
+{
+	strcpy(g.fullname, name);
+	g.handicap = hc;
+}
+
+int setgolf(golf &g)
+{
+	cout << "Enter a new name\n";
+	cin.get(g.fullname, Len);
+	cout << "Enter a handicap\n";
+	cin >> g.handicap; 
+}
+
+void handicap(golf &g, int hc)
+{
+	g.handicap = hc;
+}
+
+void showgolf(const golf &g)
+{
+	cout << g.fullname << " (" << g.handicap << ")\n";
+}
