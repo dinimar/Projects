b8196dbb930a5251d8adf71eac7f136eafc955cd
Dinir Imameev <dinir.imameev@gmail.com>
2019-06-30 18:58:21 +0500

    feat(chapter_8): add pr_ex1

---
+++ c++/primer_plus_s_pratt/ch_8/pr_ex1/app.cpp
@@ -0,0 +1,26 @@
+#ifndef GOLF_H
+#define GOLF_H
+#include <golf.h>
+#include <iostream>
+#endif
+
+int main(int argc, char const *argv[])
+{
+	golf g = {"Name", 50};
+	showgolf(g);
+
+
+	std::cout << "\nvoid setgolf():\n";
+	setgolf(g, "Boris Becker", 100);
+	showgolf(g);
+
+	std::cout << "\nint setgolf():\n";
+	setgolf(g);
+	showgolf(g);
+
+	std::cout << "\nvoid handicap():\n";
+	handicap(g, 34);
+	showgolf(g);
+
+	return 0;
+}
