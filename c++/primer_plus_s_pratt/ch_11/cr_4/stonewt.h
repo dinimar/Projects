d14ca3bd5c845c67c559de3527a572380f08c412
Dinir Imameev <dinir.imameev@gmail.com>
2019-07-04 14:46:50 +0500

    feat(chapter_11): add solution for cr_4

---
+++ c++/primer_plus_s_pratt/ch_11/cr_4/stonewt.h
@@ -0,0 +1,28 @@
+// stonewt1.h -- revised definition for the Stonewt class
+#ifndef STONEWT1_H_
+#define STONEWT1_H_
+
+class Stonewt
+{
+private:
+	enum {Lbs_per_stn = 14};
+	int stone;
+	double pds_left;
+	double pounds;
+public:
+	Stonewt(double lbs);
+	Stonewt(int stn, double lbs);
+	Stonewt();
+	~Stonewt();
+	void show_lbs() const;
+	void show_stn() const;
+
+	Stonewt operator*(double) const;
+	friend Stonewt operator*(double m, const Stonewt &s);
+
+	// conversion functions
+	operator int() const;
+	operator double() const;
+};
+
+#endif
