685973d69eb67dfe2ac271035ad2982fcac2faae
Dinir Imameev <dinir.imameev@gmail.com>
2019-07-29 12:06:40 +0500

    fix(queue): update dequeue() * add definition, tests, for isempty() * fix enqueue()

--- c++/primer_plus_s_pratt/ch_12/queue/test/src/queue/Queue.cpp
+++ c++/primer_plus_s_pratt/ch_12/queue/test/src/queue/Queue.cpp
@@ -12,7 +12,9 @@ Queue::Queue(int qs):qsize(qs) {
 
 Queue::~Queue() {}
 
-bool Queue::isempty() const {}
+bool Queue::isempty() const {
+    return (items == 0) ? true : false;
+}
 
 bool Queue::isfull() const {
     return (items == qsize) ? true : false;
@@ -22,14 +24,70 @@ int Queue::queuecount() const {
     return items;
 }
 
-bool Queue::dequeue(Item &item) {}
-
 bool Queue::enqueue(const Item &item) {
     if (items + 1 <= qsize) {
-        rear = new Node({item, nullptr});
+        if (rear == nullptr) {
+            rear = new Node({item, nullptr});
+        } else {
+            if (front == nullptr) {
+                front = new Node({item, rear});
+            } else {
+                front = new Node({item, front});
+            }
+        }
         ++items;
         return true;
     } else {
         return false;
     }
+}
+
+bool Queue::dequeue(Item &item) {
+//    if (items != 0) {
+        Node *pre_node = nullptr;
+        Node *cur_node = front;
+
+        while ((cur_node->next != nullptr) and (items > 2)) {
+            if (cur_node->item == item) {
+                // if item is in a front node
+                if (cur_node == front) {
+                    // assign next node to front
+                    front = cur_node->next;
+                    // free memory
+                    cur_node->next = nullptr;
+                    delete cur_node;
+                } else {
+                    // change address of next node
+                    pre_node->next = cur_node->next;
+                    // free memory
+                    cur_node->next = nullptr;
+                    delete cur_node;
+                }
+                // decrease amount of items
+                --items;
+                return true;
+            }
+            // assign a new pair of nodes
+            pre_node = cur_node;
+            cur_node = cur_node->next;
+        }
+
+        if (front->item == item) {
+            // free memory
+            front->next = nullptr;
+            delete front;
+
+            --items;
+
+            return true;
+        } else if (rear->item == item) {
+            // free memory
+            delete rear;
+
+            --items;
+
+            return true;
+        }
+
+        return false;
 }
--- c++/primer_plus_s_pratt/ch_12/queue/test/src/test_queue.cpp
+++ c++/primer_plus_s_pratt/ch_12/queue/test/src/test_queue.cpp
@@ -8,12 +8,16 @@ TEST(QueueTest, queuecount){
     EXPECT_EQ(q.queuecount(), 0);
 }
 
+TEST(Queue, isempty_true){
+    EXPECT_EQ(q.isempty(), true);
+}
+
 TEST(QueueTest, isfull_false){
     EXPECT_EQ(q.isfull(), false);
 }
 
 TEST(QueueTest, enqueue){
-    for(int i=0; i<10; i++){
+    for(int i=0; i<10; ++i){
         EXPECT_EQ(q.enqueue(i), true); // returns true cause there is enough space
         EXPECT_EQ(q.queuecount(),i+1); // returns current amount of items
     }
@@ -23,4 +27,18 @@ TEST(QueueTest, enqueue){
 
 TEST(QueueTest, isfull_true){
     EXPECT_EQ(q.isfull(), true);
+}
+
+TEST(QueueTest, isempy_false){
+    EXPECT_EQ(q.isempty(), false);
+}
+
+TEST(QueueTest, dequeue){
+    for(int i=9; i>=0; --i){
+        EXPECT_EQ(q.dequeue(i), true); // returns true cause there is enough space
+        EXPECT_EQ(q.queuecount(),i); // returns current amount of items
+    }
+
+    int a = 1;
+    EXPECT_EQ(q.dequeue(a), false); // returns false cause there isn't enough space
 }
