256c6fe3b4191e0449462438279fee7850a906f6
Dinir Imameev <dinir.imameev@gmail.com>
2019-12-19 14:05:05 +0300

    feat(ch12): add pe2 base version * add friend operator+

---
+++ c++/primer_plus_s_pratt/ch_12/pe_2/include/string2.h
@@ -0,0 +1,49 @@
+//
+// Created by Dinir Imameev on 11/21/19.
+// Github: @dinimar
+//
+
+#ifndef STRING2_H_
+#define STRING2_H_
+
+#include <iostream>
+
+using std::ostream;
+using std::istream;
+
+class String
+{
+private:
+    char * str;
+// pointer to string
+    int len;
+// length of string
+    static int num_strings; // number of objects
+    static const int CINLIM = 80; // cin input limit
+public:
+// constructors and other methods
+    String(const char * s); // constructor
+    String();
+// default constructor
+    String(const String &); // copy constructor
+    ~String();
+// destructor
+    int length () const { return len; }
+// overloaded operator methods
+    String & operator=(const String &);
+    String & operator=(const char *);
+//    String operator+(const String &) const;
+//    String operator+(const char *) const;
+    char & operator[](int i);
+    const char & operator[](int i) const;
+// overloaded operator friends
+    friend String operator+(char * s, const String &st);
+    friend bool operator<(const String &st, const String &st2);
+    friend bool operator>(const String &st1, const String &st2);
+    friend bool operator==(const String &st, const String &st2);
+    friend ostream & operator<<(ostream & os, const String & st);
+    friend istream & operator>>(istream & is, String & st);
+// static function
+    static int HowMany();
+};
+#endif
