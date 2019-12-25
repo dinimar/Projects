2e3a1b94b895f803731aca83d38c4fccac637602
Dinir Imameev <dinir.imameev@gmail.com>
2019-12-25 16:24:46 +0300

    feat(ch12.pe4): add push()

--- c++/primer_plus_s_pratt/ch_12/pe_4/src/test_stack.cc
+++ c++/primer_plus_s_pratt/ch_12/pe_4/src/test_stack.cc
@@ -7,11 +7,17 @@
 #include <stack.h>
 
 int main(int argc, char **argv) {
+    int size = 11;
+
     Stack st;
-    Stack cp(11);
+    Stack cp(size);
 
     st = st;
     st = cp;
+
+    for (int i = 0; i < size; ++i) {
+        st.push(Item(10-i));
+    }
 
     bool em = st.isempty();
     bool full = st.isfull();
