3a25da28e93382a0d581d134ec2d17ef69141dba
Dinir Imameev <dinir.imameev@gmail.com>
2020-01-12 00:23:21 +0300

    feat(bolt-cms): add config generation

--- python/graber/bolt_script.py
+++ python/graber/bolt_script.py
@@ -1,9 +1,27 @@
 import os
 from fixer import get_all_paths_by_level
+from fixer import extract_title
 from os.path import normpath, basename
 
 root = os.path.dirname(os.path.abspath(__file__))
 pages_path= '/home/dinir/Documents/git/graber_out/clean_pages/pages'
+
+# {0} - must be in english cause it's name of a table
+# {1} - must be in russian cause it's name of section in views
+db_config_str = """{0}:
+    name: {1}
+    singular_name: {1}
+    fields:
+        title:
+            type: text
+            class: large
+        slug:
+            type: slug
+            uses: title
+        text:
+            type: html
+            height: 300px
+    record_template: article.twig"""
 
 def get_sec_name(s_path):
     start = s_path.find('.')
@@ -11,16 +29,26 @@ def get_sec_name(s_path):
 
 if __name__ == '__main__':
     # tmp_dir = os.path.join(root, p_dir)
+    secs = [] # list contains eng[0] and rus[1] names of sections, each element is a tuple
     dir_list = get_all_paths_by_level(pages_path, 1)
     # secs = sort_by_default(dir_list)
-    secs = {}
-    # for each category create table(update config.yml int a+ mode)
-    # and add file table.txt
-    for dir_path in dir_list:
-        secs[get_sec_name(sub_dir)] = dir_path
-        # category
 
-    # rename_sections(secs)
-    # [x[0] for x in os.walk(os.path.join(root, p_dir))]
-    # dir_l = os.walk(os.path.join(root, p_dir))
-    pass
+    # for each category add file table.txt
+    for dir_path in dir_list:
+        sec_name = get_sec_name(basename(normpath(dir_path))) # name in english
+        
+        # Get title from default.md
+        with open(os.path.join(dir_path, 'blog.md'), 'r') as blog_file:
+            blog_file.readline() # skip a first line
+            secs.append((sec_name.replace('-',''), extract_title(blog_file.readline())))
+
+        # Create table.txt with table name
+        with open(os.path.join(dir_path, 'table.txt'), 'w+') as f:
+            f.write(sec_name)
+        
+    # Add configurations to config.yml
+    with open(os.path.join(pages_path, 'config.yml'), 'w+') as f:
+        for sec in secs:
+            f.write(db_config_str.format(sec[0], sec[1]))
+            f.write('\n\n')
+
