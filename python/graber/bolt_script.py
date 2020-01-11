871f9f620c4a4244e4d9fe25b8763264d98cf92d
Dinir Imameev <dinir.imameev@gmail.com>
2020-01-12 02:00:12 +0300

    feat(db-gen): add mysql connection

--- python/graber/bolt_script.py
+++ python/graber/bolt_script.py
@@ -1,9 +1,17 @@
 import os
+import configparser
+import mysql.connector
+
 from fixer import get_all_paths_by_level
 from fixer import extract_title
 from os.path import normpath, basename
 
+# db connection config
 root = os.path.dirname(os.path.abspath(__file__))
+config_file = "config.ini"
+config = configparser.ConfigParser()
+config.read(os.path.join(root, config_file))
+
 pages_path= '/home/dinir/Documents/git/graber_out/clean_pages/pages'
 
 # {0} - must be in english cause it's name of a table
@@ -27,27 +35,41 @@ def get_sec_name(s_path):
     start = s_path.find('.')
     return s_path[start+1:]
 
-if __name__ == '__main__':
-    # tmp_dir = os.path.join(root, p_dir)
-    secs = [] # list contains eng[0] and rus[1] names of sections, each element is a tuple
-    dir_list = get_all_paths_by_level(pages_path, 1)
-    # secs = sort_by_default(dir_list)
+# DB initialization
+# if __name__ == '__main__':
+#     # tmp_dir = os.path.join(root, p_dir)
+#     secs = [] # list contains eng[0] and rus[1] names of sections, each element is a tuple
+#     dir_list = get_all_paths_by_level(pages_path, 1)
+#     # secs = sort_by_default(dir_list)
 
-    # for each category add file table.txt
-    for dir_path in dir_list:
-        sec_name = get_sec_name(basename(normpath(dir_path)))[0:50] # name in english
+#     # for each category add file table.txt
+#     for dir_path in dir_list:
+#         sec_name = get_sec_name(basename(normpath(dir_path)))[0:50] # name in english
         
-        # Get title from default.md
-        with open(os.path.join(dir_path, 'blog.md'), 'r') as blog_file:
-            blog_file.readline() # skip a first line
-            secs.append((sec_name.replace('-',''), extract_title(blog_file.readline())))
+#         # Get title from default.md
+#         with open(os.path.join(dir_path, 'blog.md'), 'r') as blog_file:
+#             blog_file.readline() # skip a first line
+#             secs.append((sec_name.replace('-',''), extract_title(blog_file.readline())))
 
-        # Create table.txt with table name
-        with open(os.path.join(dir_path, 'table.txt'), 'w+') as f:
-            f.write('bolt_'+sec_name)
+#         # Create table.txt with table name
+#         with open(os.path.join(dir_path, 'table.txt'), 'w+') as f:
+#             f.write('bolt_'+sec_name)
         
-    # Add configurations to config.yml
-    with open(os.path.join(pages_path, 'config.yml'), 'w+') as f:
-        for sec in secs:
-            f.write(db_config_str.format(sec[0], sec[1]))
-            f.write('\n\n')
+#     # Add configurations to config.yml
+#     with open(os.path.join(pages_path, 'config.yml'), 'w+') as f:
+#         for sec in secs:
+#             f.write(db_config_str.format(sec[0], sec[1]))
+#             f.write('\n\n')
+
+
+
+if __name__ == "__main__":
+    # dir_list = get_all_paths_by_level(pages_path, 1)
+    
+    # Connect to bolt_db
+    cnx = mysql.connector.connect(user=config['bolt_db']['user'], password=config['bolt_db']['password'],
+                              host=config['bolt_db']['host'],
+                              database=config['bolt_db']['database'])
+    cnx.close()
+
+    pass
