b711b9e4699649c898f6be1d64a5b2a0db6ca6bd
Dinir Imameev <dinir.imameev@gmail.com>
2020-03-01 11:58:42 +0300

    refactor(test): move to sepate dir * add include_dir in CMake config

--- c++/cyber_security/test/test_des.cc
+++ c++/cyber_security/test/test_des.cc
@@ -1,8 +1,10 @@
 #define BOOST_TEST_MODULE test des
 #include <boost/test/included/unit_test.hpp>
+#include "des.h"
 
 BOOST_AUTO_TEST_CASE( free_test_function )
 /* Compare with void free_test_function() */
 {
+  DES des("123");
   BOOST_TEST(true);
 }
