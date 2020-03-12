11891a11db21dfd7d801b6c3dd31eed741cd6c12
Dinir Imameev <dinir.imameev@gmail.com>
2020-03-12 17:19:06 +0300

    feat(des): add permutation tables

--- c++/cyber_security/src/des.cc
+++ c++/cyber_security/src/des.cc
@@ -1,15 +1,16 @@
 #include <vector>
 #include "des.h"
 
-DES::DES(std::array<int8_t, 64> key)
+DES::DES(std::array<int8_t, 7> key)
 {
     if (key.size() == BYTES_NUM)    // if passed key length is 7 bytes 
     {
         // using copy constructor 
         // and assignment operator to inititalize
-        key_ = key;                                 // initialize by passed key
+        key_.swap(key);                                 // initialize by passed key
     } else                          
     {
-        key_ = std::array<int8_t, BYTES_NUM>();   // initialize by zeros
+        key_ = std::array<int8_t, BYTES_NUM>();   
+        key_.fill(0);   // initialize by zeros
     }
 }
