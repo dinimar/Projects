fa88cc2e63648676393943172f021b1a99404c58
Dinir Imameev <dinir.imameev@gmail.com>
2020-03-12 19:11:31 +0300

    feat(des): add permutations * add encrypt() * update test

--- c++/cyber_security/src/des.cc
+++ c++/cyber_security/src/des.cc
@@ -1,16 +1,38 @@
-#include <vector>
 #include "des.h"
 
-DES::DES(std::array<int8_t, 7> key)
+int64_t DES::get_nth_bit(int n, int64_t data)
 {
-    if (key.size() == BYTES_NUM)    // if passed key length is 7 bytes 
+    return (data >> n) & 1; // shift bits and mask
+}
+
+int64_t DES::init_permutate(int64_t data)
+{
+    int64_t perm_data = 0;  // zero-initialized permutated data
+    int k = 0;              // multiplier for bits
+
+    for (int & a: init_table)
     {
-        // using copy constructor 
-        // and assignment operator to inititalize
-        key_.swap(key);                                 // initialize by passed key
-    } else                          
-    {
-        key_ = std::array<int8_t, BYTES_NUM>();   
-        key_.fill(0);   // initialize by zeros
+        perm_data += get_nth_bit(k, data) << a; // bit shifting by 0, 1 till 63 bits
+        ++k;
     }
 }
+
+int64_t DES::final_permutate(int64_t data)
+{
+    int64_t perm_data = 0;  // zero-initialized permutated data
+    int k = 0;              // multiplier for bits
+
+    for (int & a: final_table)
+    {
+        perm_data += get_nth_bit(k, data) << a; // bit shifting by 0, 1 till 63 bits
+        ++k;
+    }
+}
+
+int64_t DES::encrypt(int64_t data)
+{
+    int64_t init_perm_data = init_permutate(data);    // perform initial permutation
+    int64_t final_perm_data = final_permutate(data);
+    
+    return final_perm_data;
+};
--- c++/cyber_security/test/test_des.cc
+++ c++/cyber_security/test/test_des.cc
@@ -4,7 +4,10 @@
 
 BOOST_AUTO_TEST_CASE( des_encrypt )
 {
-  std::vector<int8_t> key = {1, 2, 3, 4, 5, 6, 7};
-  DES des(key);
-  BOOST_TEST(true);
+  int64_t key = 0x0101010101010101;
+  int64_t data = 0x95F8A5E5DD31D900;
+  
+  DES des(key);  // initialize object
+
+  BOOST_CHECK_BITWISE_EQUAL(0x8000000000000000, des.encrypt(data));
 }
