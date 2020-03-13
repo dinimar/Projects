e529bfea7196a3e73f899fc6280d3c5d05e9213c
Dinir Imameev <dinir.imameev@gmail.com>
2020-03-13 12:25:33 +0300

    feat(des): add p_table for key permutation

--- c++/cyber_security/src/des.cc
+++ c++/cyber_security/src/des.cc
@@ -49,10 +49,17 @@ int64_t DES::encrypt(int64_t data)
     int32_t left_data = (init_perm_data & left_mask) >> 32; // needs extra shifting to reduce size to 32 bits
     int32_t right_data = init_perm_data & right_mask;
 
-    // e_func
-    int64_t out_data_left = permutate(left_data, r_block_table) // perform permutation 
-                            ^ key_                              // XOR with key
-                            << 16 >> 16;                         // remove first 16 bits that added after XOR
+    // generate round keys
+
+
+    // 15 rounds of encryption
+    for(size_t i=0; i<15; ++i)
+    {
+        // e_func
+        int64_t out_data_left = permutate(left_data, r_block_table) // perform permutation 
+                                ^ key_                              // XOR with key
+                                << 16 >> 16;                         // remove first 16 bits that added after XOR
+    }
                         
     int64_t out_data_right = permutate(right_data, r_block_table);
 
