9ae9b8c74f0aa0b901574f039843fdc8f6a62ca7
Dinir Imameev <dinir.imameev@gmail.com>
2020-04-23 09:28:21 +0300

    refactor(cryptopals.33): add modexp()

--- c++/cyber_security/cryptopals/33.Diffie-Hellman/src/main.cc
+++ c++/cyber_security/cryptopals/33.Diffie-Hellman/src/main.cc
@@ -3,22 +3,33 @@
 #include <cmath>
 #include <cassert>
 
+uint64_t modexp(uint64_t base, uint64_t exp, uint64_t modNum)
+{
+    while (exp != 0)
+    {
+        base = base * exp;
+        exp--;
+    }
+
+    return base % modNum;
+}
+
 int main(int argc, char** argv)
 {
     std::srand(std::time(nullptr));     // use current time as seed for random generator
 
-    int p = 37;     // set p to 37
-    int g = 5;      // set g to 5
+    uint64_t p = 37;     // set p to 37
+    uint64_t g = 5;      // set g to 5
 
-    int a = std::rand() % 37;        // generate a, random int % 37         
-    int A = int(std::pow(double(g), double(a))) % p;    // generate A = (g**a) % p
+    uint64_t a = std::rand() % 37;        // generate a, random int % 37         
+    uint64_t A = modexp(g, a, p);    // generate A = (g**a) % p
 
-    int b = std::rand() % 37;        // generate b, random int % 37         
-    int B = int(std::pow(double(g), double(b))) % p;    // generate B = (g**b) % p
+    uint64_t b = std::rand() % 37;        // generate b, random int % 37         
+    uint64_t B = modexp(g, b, p);    // generate B = (g**b) % p
 
 
-    int sA = int(std::pow(double(B), double(a))) % p;   // generate session key, s = (B**a) % p
-    int sB = int(std::pow(double(A), double(b))) % p; 
+    uint64_t sA = modexp(B, a, p);   // generate session key, s = (B**a) % p
+    uint64_t sB = modexp(A, b, p);   // generate session key, s = (A**b) % p
 
     assert(sA == sB);
 
