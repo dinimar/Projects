841ed9d7ef1dd76d6c71602e0d94bf0eb76eb19d
Dinir Imameev <dinir.imameev@gmail.com>
2020-04-24 13:24:15 +0300

    refactor(c++): add separate dir for c++

---
+++ c++/cyber_security/cryptopals/Set-5/c++/src/main.cc
@@ -0,0 +1,31 @@
+#include "diffie-hellman.h"
+#include <openssl/sha.h>
+#include <openssl/aes.h>
+
+static const char alphanum[] =
+"0123456789"
+"!@#$%^&*"
+"ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+"abcdefghijklmnopqrstuvwxyz";
+
+int stringLength = sizeof(alphanum) - 1;
+
+char genRandom()
+{
+    return alphanum[rand() % stringLength];
+}
+
+
+int main(int argc, char** argv)
+{
+    // Challenge 33
+    using boost::multiprecision::cpp_int;
+    using namespace ENCRYPTION::DIFFIE_HELLMAN;
+
+    cpp_int p = 0xffffffffffffffffc90fdaa22168c234c4c6628b80dc1cd129024e088a67cc74020bbea63b139b22514a08798e3404ddef9519b3cd3a431b302b0a6df25f14374fe1356d6d51c245e485b576625e7ec6f44c42e9a637ed6b0bff5cb6f406b7edee386bfb5a899fa5ae9f24117c4b1fe649286651ece45b3dc2007cb8a163bf0598da48361c55d39a69163fa8fd24cf5f83655d23dca3ad961c62f356208552bb9ed529077096966d670c354e4abc9804f1746c08ca237327ffffffffffffffff; 
+    cpp_int g = 2;
+
+    publicSessionKey(p, g);
+
+    return 0;
+}
